7c57885de096db60d1300c65fa277ff645103d66
Harsh Gupta <harsh@helpshift.com>
2020-10-25 00:08:00 +0530

    read creds from enviornment variable

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+import os
 import gspread
 from flask import Flask
 
 def init():
-    gc = gspread.service_account()
+    creds_dict = eval(os.getenv("GSHEETS_CREDS"))
+    gc = gspread.service_account_from_dict(creds_dict)
     return gc.open("Ping Counter").sheet1
-
 
 def read_count(gs, count_cell):
     return int(gs.get(count_cell).first())
@@ -14,11 +15,17 @@ def increment_count(gs, count_cell):
     gs.update(count_cell, new_count)
     return new_count
 
-App = Flask(__name__)
+
+# =============== Main ====================
+
 Sheet = init()
 count_cell = "B1"
 assert Sheet
 
+
+# ---------- Webserver --------------------
+
+App = Flask(__name__)
 @App.route('/')
 def counter():
     return increment_count(Sheet, count_cell)
